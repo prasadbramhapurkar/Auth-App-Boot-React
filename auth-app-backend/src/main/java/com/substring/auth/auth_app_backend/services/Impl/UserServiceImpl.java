3586d7e71792149d8175e4f446205a3b674377de
prasadbramhapurkar <prasadbramhapurkar1234@gmail.com>
2026-06-14 19:51:58 +0530

    register API implementation

--- auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/services/Impl/UserServiceImpl.java
+++ auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/services/Impl/UserServiceImpl.java
@@ -1,4 +1,4 @@
-package com.substring.auth.auth_app_backend.services;
+package com.substring.auth.auth_app_backend.services.Impl;
 
 import com.substring.auth.auth_app_backend.dtos.UserDto;
 import com.substring.auth.auth_app_backend.entities.Provider;
@@ -6,6 +6,7 @@ import com.substring.auth.auth_app_backend.entities.User;
 import com.substring.auth.auth_app_backend.exceptions.ResourceNotFoundException;
 import com.substring.auth.auth_app_backend.helpers.UserHelper;
 import com.substring.auth.auth_app_backend.repositories.UserRepository;
+import com.substring.auth.auth_app_backend.services.UserService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -15,7 +16,7 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
